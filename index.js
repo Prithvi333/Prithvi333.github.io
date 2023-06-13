3d1243246bf2a707d31bdce6614bd2e6303e8c1e
Prithvi Chauhan <pthakur.pt36@gmail.com>
2023-06-13 11:07:47 +0530

    portfolio updated

--- index.js
+++ index.js
@@ -4,55 +4,55 @@ toggle.addEventListener("click", function () {
   this.classList.toggle("bi-moon-fill");
   if (this.classList.toggle("bi-brightness-high")) {
     body.style.background = "#080808";
-    body.style.transition = "2s";
+    body.style.transition = "1s";
     document.getElementById("user-detail-name").style.color = "orangered";
-    document.getElementById("user-detail-name").style.transition = "2s";
+    document.getElementById("user-detail-name").style.transition = "1s";
     document.querySelector("#one>.text-1").style.color = "white";
-    document.querySelector("#one>.text-1").style.transition = "2s";
+    document.querySelector("#one>.text-1").style.transition = "1s";
     document.querySelector("#one>.text-3>span").style.color = "white";
-    document.querySelector("#one>.text-3>span").style.transition = "2s";
+    document.querySelector("#one>.text-3>span").style.transition = "1s";
     document.querySelector("#resume-link-2").style.color = "white";
-    document.querySelector("#resume-button-2").style.transition = "2s";
+    document.querySelector("#resume-button-2").style.transition = "1s";
     document.querySelector("#resume-button-2").style.border =
       "1px solid orangered";
     document.querySelector("#about>h1").style.color = "white";
-    document.querySelector("#one>.text-3>span").style.transition = "2s";
+    document.querySelector("#one>.text-3>span").style.transition = "1s";
     document.querySelector("#user-detail-intro").style.color = "white";
-    document.querySelector("#user-detail-intro").style.transition = "2s";
+    document.querySelector("#user-detail-intro").style.transition = "1s";
     document.querySelector("#skills>h1").style.color = "white";
-    document.querySelector("#skills>h1").style.transition = "2s";
+    document.querySelector("#skills>h1").style.transition = "1s";
     document.querySelector("#projects>h1").style.color = "white";
-    document.querySelector("#projects>h1").style.transition = "2s";
+    document.querySelector("#projects>h1").style.transition = "0s";
     document.querySelector(".github-activity-calendar h1").style.color =
       "white";
     document.querySelector(".github-activity-calendar h1").style.transition =
-      "2s";
+      "1s";
   } else {
     body.style.background = "white";
-    body.style.transition = "2s";
+    body.style.transition = "1s";
     document.getElementById("user-detail-name").style.color = "RGB(35, 40, 62)";
-    document.getElementById("user-detail-name").style.transition = "2s";
+    document.getElementById("user-detail-name").style.transition = "1s";
     document.querySelector("#one>.text-1").style.color = "#5566a1";
-    document.querySelector("#one>.text-1").style.transition = "2s";
+    document.querySelector("#one>.text-1").style.transition = "1s";
     document.querySelector("#one>.text-3>span").style.color = "#5566a1";
-    document.querySelector("#one>.text-3>span").style.transition = "2s";
+    document.querySelector("#one>.text-3>span").style.transition = "1s";
     document.querySelector("#resume-button-2>a").style.color =
       "RGB(35, 40, 62)";
-    document.querySelector("#resume-button-2").style.transition = "2s";
+    document.querySelector("#resume-button-2").style.transition = "1s";
     document.querySelector("#resume-button-2").style.border =
       "1px solid RGB(35, 40, 62)";
     document.querySelector("#about>h1").style.color = "RGB(35, 40, 62)";
-    document.querySelector("#about>h1").style.transition = "2s";
+    document.querySelector("#about>h1").style.transition = "1s";
     document.querySelector("#user-detail-intro").style.color = "#5566a1";
-    document.querySelector("#user-detail-intro").style.transition = "2s";
+    document.querySelector("#user-detail-intro").style.transition = "1s";
     document.querySelector("#skills>h1").style.color = "RGB(35, 40, 62)";
-    document.querySelector("#skills>h1").style.transition = "2s";
+    document.querySelector("#skills>h1").style.transition = "1s";
     document.querySelector("#projects>h1").style.color = "RGB(35, 40, 62)";
-    document.querySelector("#projects>h1").style.transition = "2s";
+    document.querySelector("#projects>h1").style.transition = "0s";
     document.querySelector(".github-activity-calendar>h1").style.color =
       "RGB(35, 40, 62)";
     document.querySelector(".github-activity-calendar>h1").style.transition =
-      "2s";
+      "1s";
   }
 });
 
@@ -66,7 +66,7 @@ function closemenu() {
 }
 // typing text animation script
 var typed = new Typed(".typing", {
-  strings: ["A Full Stack Web Developer", "A JAVA Backend Developer"],
+  strings: ["A Full Stack Developer", "A JAVA Backend Developer"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
@@ -74,24 +74,26 @@ var typed = new Typed(".typing", {
 
 document.querySelector("#resume-link-1").addEventListener("click", () => {
   // console.log("OPENinig.....")
-  window.location.assign(
-    "https://drive.google.com/file/d/1Rb0N0jNgYBqA-wxkw8QDu7W65-0kkb9U/view?usp=sharing",
+  // window.location.assign("https://drive.google.com/file/d/1cHRUp1zZ59hIw7UuIul2T14R8IdjhDXv/view?usp=sharing", "_blank");
+  window.open(
+    "https://drive.google.com/file/d/1cHRUp1zZ59hIw7UuIul2T14R8IdjhDXv/view?usp=sharing",
     "_blank"
   );
 });
 
 document.querySelector("#resume-link-2").addEventListener("click", () => {
   // console.log("OPENinig.....")
-  window.location.assign(
-    "https://drive.google.com/file/d/1Rb0N0jNgYBqA-wxkw8QDu7W65-0kkb9U/view?usp=sharing",
+  // window.location.assign("https://drive.google.com/file/d/1cHRUp1zZ59hIw7UuIul2T14R8IdjhDXv/view?usp=sharing", "_blank");
+  window.open(
+    "https://drive.google.com/file/d/1cHRUp1zZ59hIw7UuIul2T14R8IdjhDXv/view?usp=sharingom",
     "_blank"
   );
 });
 
 const sr = ScrollReveal({
   origin: "left",
-  distance: "120px",
-  duration: 2000,
+  distance: "60px",
+  duration: 300,
   reset: true,
 });
 
